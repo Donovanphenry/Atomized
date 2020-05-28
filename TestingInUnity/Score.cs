6532c514df9316d752ca132287f4a20e3d9f85da
Donovanphenry <48369383+Donovanphenry@users.noreply.github.com>
2020-05-28 15:53:34 -0700

    Player position display
    The player's position will now display at the middle of the screen.

--- TestingInUnity/Score.cs
+++ TestingInUnity/Score.cs
@@ -1,1 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
 
+public class Score : MonoBehaviour {
+
+    public Transform player;
+    public Text playerScore;
+
+	// Update is called once per frame
+	void Update () {
+        // Sets a text passed into this object to represent a player's
+        // position along the z-axis
+        playerScore.text = ((int)player.position.z).ToString() + " [m]";
+	}
+}
